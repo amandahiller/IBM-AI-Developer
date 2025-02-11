2ce58b3bbf12568f0d10c3e72d53561014b5100f
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2025-02-11 15:55:45 -0800

    code fix
    Signed-off-by: Amanda Speroni-Hiller <amandasperoni@icloud.com>

--- chatbot.py
+++ chatbot.py
@@ -1,37 +1,39 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from flask import request
+from flask import Flask, request, render_template
+from flask_cors import CORS
 import json
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+app = Flask(__name__)
+CORS(app)
 
 model_name = "facebook/blenderbot-400M-distill"
-
-# Load model (download on first run and reference local installation for consequent runs)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-#Keeping track of conversation history
 conversation_history = []
 
 @app.route('/chatbot', methods=['POST'])
 def handle_prompt():
-    # Read prompt from HTTP request body
     data = request.get_data(as_text=True)
     data = json.loads(data)
     input_text = data['prompt']
 
-    #Encoding the conversation history
-    history_string = "\n".join(conversation_history)
+    # Create conversation history string
+    history = "\n".join(conversation_history)
 
-    #Tokenization of user prompt and chat history
-    inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
+    # Tokenize the input text and history
+    inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
 
-    #Generate output from the model
-    outputs = model.generate(**inputs, max_length= 60)  # max_length will acuse model to crash at some point as history grows
+    # Generate the response from the model
+    outputs = model.generate(**inputs, max_length= 60)  # max_length will cause the model to crash at some point as history grows
 
-    #Decode output
+    # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
 
-    #Update conversation history
+    # Add interaction to conversation history
     conversation_history.append(input_text)
     conversation_history.append(response)
 
     return response
+
+if __name__ == '__main__':
+    app.run()
