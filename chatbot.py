1e435e52b1e553d47a7069f16ee253f7e266b871
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2025-02-11 15:12:54 -0800

    created converstation history list
    Signed-off-by: Amanda Speroni-Hiller <amandasperoni@icloud.com>

--- chatbot.py
+++ chatbot.py
@@ -5,3 +5,5 @@ model_name = "facebook/blenderbot-400M-distill"
 # Load model (download on first run and reference local installation for consequent runs)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+conversation_history = []
