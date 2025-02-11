a11ad2610f34e076c06dac46b702de28c7b6af33
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2025-02-11 15:20:07 -0800

    changes
    Signed-off-by: Amanda Speroni-Hiller <amandasperoni@icloud.com>

--- chatbot.py
+++ chatbot.py
@@ -22,3 +22,7 @@ print(outputs)
 
 response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
 print(response)
+
+conversation_history.append(input_text)
+conversation_history.append(response)
+print(conversation_history)
